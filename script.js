cd3167a70b751a80f93171972993b69bba340f44
Nagamani Gaatala <57241062+nagamanigaatala@users.noreply.github.com>
2023-12-18 22:23:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+// Toggle icon navbar
+let menuIcon = document.getElementById('menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+// Scroll sections
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    let top = window.scrollY;
+
+    sections.forEach((section) => {
+        let offset = section.offsetTop - 100;
+        let height = section.offsetHeight;
+        let id = section.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            // Active navbar links
+            navLinks.forEach((link) => {
+                link.classList.remove('active');
+                if (link.getAttribute('href') === `#${id}`) {
+                    link.classList.add('active');
+                }
+            });
+        }
+    });
+};
